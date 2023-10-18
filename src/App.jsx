5826f5fe85dffa904d0731b9bbc256e45c995adc
Rea <andrea.solisg@gmail.com>
2023-10-18 22:12:27 +0200

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-
-import './App.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-
   return (
-    <>
-Hello !
-    </>
-  )
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
